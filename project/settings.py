b0d6653830eed4586733095cfc710263b2ef66f8
Mohamed Gouda <goda61@gmail.com>
2024-03-30 01:23:25 +0200

    Design product app models

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Installed Packages :
+    'taggit',
+
+
+    # My Apps :
+    'products',
 ]
 
 MIDDLEWARE = [
