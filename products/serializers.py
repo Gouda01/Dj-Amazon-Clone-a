65d74a8a8060b51b1ebcac020d7737a2da9552e1
Mohamed Gouda <goda61@gmail.com>
2024-04-04 13:03:48 +0200

    Add proberty to method in model

--- products/serializers.py
+++ products/serializers.py
@@ -17,50 +17,24 @@ class ProductReviewSerializer(serializers.ModelSerializer):
 
 class ProductListSerializer(serializers.ModelSerializer) :
     brand = serializers.StringRelatedField()
-    review_count = serializers.SerializerMethodField()
-    avg_rate = serializers.SerializerMethodField()
-
 
     class Meta:
         model = Product
-        fields = '__all__'
-
-    def get_review_count(self,object):
-        reviews = object.review_count()
-        return reviews
-    
-    def get_avg_rate(self, object):
-        avg = object.avg_rate()
-        return avg
+        # fields = '__all__'
+        fields = ['id','name','flag','price','image','sku','subtitle','description','brand','review_count','avg_rate']
 
 
 class ProductDetailSerializer(serializers.ModelSerializer) :
     brand = serializers.StringRelatedField()
-    review_count = serializers.SerializerMethodField()
-    avg_rate = serializers.SerializerMethodField()
-
     images = ProductImageSerializer(source='product_image', many=True)
     reviews = ProductReviewSerializer(source='review_product', many=True)
 
     class Meta:
         model = Product
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ['id','name','flag','price','image','images','sku','subtitle','description','brand','reviews','review_count','avg_rate']
 
-    def get_review_count(self,object):
-        reviews = object.review_product.all().count()
-        return reviews
     
-    def get_avg_rate(self, object):
-        total = 0
-        reviews = object.review_product.all()
-        if len(reviews) > 0 :
-            for item in reviews :
-                total += item.rate
-
-            avg = total / len(reviews)
-        else :
-            avg = 0
-        return avg
 
     
     
