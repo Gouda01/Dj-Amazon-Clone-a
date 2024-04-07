1ec2889ec587b4e5c7410da558e7eea0af517495
Mohamed Gouda <goda61@gmail.com>
2024-04-07 22:41:59 +0200

    Show data in checkout page

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 
 from .models import Order, OrderDetail, Cart, CartDetail, Coupon
 from products.models import Product
+from settings.models import DeliveryFee
 
 
 
@@ -13,8 +14,23 @@ def order_list(request):
 
 
 def checkout(request):
+    cart = Cart.objects.get(user=request.user, status='Inprogress')
+    cart_detail = CartDetail.objects.filter(cart=cart)
+    delivery_fee = DeliveryFee.objects.last().fee
 
-    return render (request, 'orders/checkout.html',{})
+    sub_total = cart.cart_total
+    discount = 0
+    total = sub_total + delivery_fee
+
+    return render (request, 'orders/checkout.html',{
+        'cart_detail' : cart_detail,
+        'delivery_fee' : delivery_fee,
+        'sub_total' : sub_total,
+        'discount' : discount,
+        'total' : total,
+    })
+
+
 
 def add_to_cart(request) :
     product = Product.objects.get(id= request.POST['product_id'])
