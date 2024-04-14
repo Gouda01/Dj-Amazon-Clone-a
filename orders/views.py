88cb1cb1b86a683a9c1487c731b8f56b759389b8
Mohamed Gouda <goda61@gmail.com>
2024-04-14 17:24:10 +0200

    Add coupon using ajax in checkout template

--- orders/views.py
+++ orders/views.py
@@ -23,6 +23,48 @@ def checkout(request):
     delivery_fee = DeliveryFee.objects.last().fee
 
 
+    # Add Coupon Befor ajax :
+    # if request.method == 'POST' :
+    #     code = request.POST['coupon_code']
+    #     # coupon = Coupon.objects.get(code=code)
+    #     coupon = get_object_or_404(Coupon,code=code)
+
+    #     if coupon and coupon.quantity > 0 :
+    #         today_date = datetime.datetime.today().date()
+    #         if today_date >= coupon.start_date and today_date <= coupon.end_date :
+    #             coupon_value = round(cart.cart_total / 100 * coupon.discount,2)
+    #             sub_total = cart.cart_total - coupon_value
+    #             total = sub_total + delivery_fee
+
+    #             cart.coupon = coupon
+    #             cart.total_with_coupon = sub_total
+    #             cart.save()
+
+    #             coupon.quantity -= 1
+    #             coupon.save()
+
+    #             return render (request, 'orders/checkout.html',{
+    #             'cart_detail' : cart_detail,
+    #             'delivery_fee' : delivery_fee,
+    #             'sub_total' : sub_total,
+    #             'discount' : coupon_value,
+    #             'total' : total,
+    #         })
+
+    # sub_total = cart.cart_total
+    # discount = 0
+    # total = sub_total + delivery_fee
+
+    # return render (request, 'orders/checkout.html',{
+    #     'cart_detail' : cart_detail,
+    #     'delivery_fee' : delivery_fee,
+    #     'sub_total' : sub_total,
+    #     'discount' : discount,
+    #     'total' : total,
+    #     })
+
+
+    # Add Coupon Using ajax :
     if request.method == 'POST' :
         code = request.POST['coupon_code']
         # coupon = Coupon.objects.get(code=code)
@@ -42,23 +84,38 @@ def checkout(request):
                 coupon.quantity -= 1
                 coupon.save()
 
-                return render (request, 'orders/checkout.html',{
-                'cart_detail' : cart_detail,
-                'delivery_fee' : delivery_fee,
-                'sub_total' : sub_total,
-                'discount' : coupon_value,
-                'total' : total,
-            })
+                # Get data after save :
+                cart = Cart.objects.get(user=request.user, status='Inprogress')
+                cart_detail = CartDetail.objects.filter(cart=cart)
+                sub_total = cart.cart_total
+                if cart.coupon :
+                    coupon_value = round(cart.cart_total / 100 * cart.coupon.discount,2)
+                else :
+                    coupon_value = 0
+                total = sub_total + delivery_fee
+
+                page = render_to_string('includes/checkout_detail.html', {
+                    'cart_detail':cart_detail,
+                    'delivery_fee':delivery_fee,
+                    'sub_total':sub_total,
+                    'discount':coupon_value,
+                    'total':total,
+                    })
+                return JsonResponse({'result':page})
 
     sub_total = cart.cart_total
-    discount = 0
+    if cart.coupon :
+        coupon_value = round(cart.cart_total / 100 * cart.coupon.discount,2)
+    else :
+        coupon_value = 0
+
     total = sub_total + delivery_fee
 
     return render (request, 'orders/checkout.html',{
         'cart_detail' : cart_detail,
         'delivery_fee' : delivery_fee,
         'sub_total' : sub_total,
-        'discount' : discount,
+        'discount' : coupon_value,
         'total' : total,
         })
 
