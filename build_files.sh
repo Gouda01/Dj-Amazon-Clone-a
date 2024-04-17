a9f362a234cb5ac4c333d11ad68031de8109e786
Mohamed Gouda <goda61@gmail.com>
2024-04-17 23:16:48 +0200

    Fix vercel settings for deploy

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,16 @@
-echo "Build Start"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic  --noinput --clear
+# Create a virtual environment
+echo "Creating a virtual environment..."
+python3.9 -m venv venv
+source venv/bin/activate
+
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
+
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
 echo "Build End"
