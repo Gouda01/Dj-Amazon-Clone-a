688aa2d9de6950b1547e1229151fa03133573521
Mohamed Gouda <goda61@gmail.com>
2024-04-17 23:33:04 +0200

    Fix vercel settings for deploy

--- project/settings.py
+++ project/settings.py
@@ -173,8 +173,12 @@ STATIC_URL = 'static/'
 #     BASE_DIR / "static",
 # ]
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static'),
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / "media"
